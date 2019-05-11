660c3d019fa7b96a5d55c1b954a8048a79ee7b04
jordanjpark <jordanjpark@gmail.com>
2019-05-11 05:34:11 -0400

    initial set up from hw 1 and API for addUser

--- README.md
+++
@@ -1,8 +0,0 @@
-## This project is an assignment for Columbia's Cloud Computing and Big Data class
-##### DESCRIPTION
-
-Authors: Sin-Yin Huang (sh3907), Zhi Ji (zj2242), Yida Lin (yl3842), Jordan Park (jjp2181)
-
-`npm install` - `npm run build` - `npm run start` to demo locally.
-
->Please go to [this link](http://smart-photo-album.s3-website-us-east-1.amazonaws.com) to test the application
---
+++ restaurant-meetup-api/addUser.js
@@ -0,0 +1,25 @@
+import uuid from "uuid";
+import * as dynamoDbLib from "./libs/dynamodb-lib";
+import { success, failure } from "./libs/response-lib";
+
+
+export async function main(event, context, callback) {
+  // Request body is passed in as a JSON encoded string in 'event.body'
+  const data = JSON.parse(event.body);
+
+  const params = {
+    TableName: "restaurant-meetup-users",
+    Item: {
+      userId: data.userId,
+      address: data.address,
+    }
+  };
+
+  try {
+    await dynamoDbLib.call("put", params);
+    return success(params.Item);
+  } catch (e) {
+    console.log(e)
+    return failure({ status: false });
+  }
+}
